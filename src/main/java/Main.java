6b73dd47bb2411c486f77caaf81fba2aed750529
Yulya Evgrafova <yulyaevrafova92@gmail.com>
2020-04-17 16:09:20 +0300

    Added visitor's code + edited grammar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,28 @@
+import gen.GrammarLexer;
+import gen.GrammarParser;
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        try {
+            ANTLRInputStream input = new ANTLRFileStream("D:\\yapis\\ListCompiler\\src\\test\\file1.txt");
+            GrammarLexer lexer = new GrammarLexer(input);
+            GrammarParser parser = new GrammarParser(new CommonTokenStream(lexer));
+            ParseTree tree = parser.program();
+            CompilerVisitor visitor = new CompilerVisitor();
+            String output = (String) visitor.visit(tree);
+
+            FileWriter fileWriter = new FileWriter("Program.java");
+            fileWriter.write(output);
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
